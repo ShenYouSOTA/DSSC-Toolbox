31b7c0b5c8965868305889b77de4de3b4fc3a9f4
EX-AID <157606841+EX-AID-0@users.noreply.github.com>
2026-07-10 21:29:25 +0800

    Delete helpers/gaiax-local-trust directory

--- helpers/gaiax-local-trust/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-cp -R /development $OUTPUT_FOLDER/development
-echo "selfSignedRoot: ${ROOT_CA}" > /data.yaml
-mustache /data.yaml /trust-anchors.xml.mustache > $OUTPUT_FOLDER/development/trust-anchors/trust-anchors.xml
